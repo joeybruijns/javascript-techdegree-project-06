ab3f970e1e5dd6e09d9102c01ed97e1bb217b4b6
Joey Bruijns <39805974+joeybruijns@users.noreply.github.com>
2020-02-08 15:46:31 +0100

    Added default error handlers and updated data.json

--- app.js
+++ app.js
@@ -6,25 +6,45 @@ const {data} = require('./data.json');
 // express() returns an Express application
 const app = express();
 
+// link the to the static files
+app.use('/static', express.static('public'));
+
 // use Pug as the view engine
 app.set('view engine', 'pug');
 
-// serve files from the public directory
-app.use(express.static('public'));
-
+// homepage route
 app.get('/', (req, res) => {
-    console.log('home');
+    console.log('home'); // DELETE
+    res.render('index');
 });
 
+// about page route
 app.get('/about', (req, res) => {
-    console.log('about');
+    console.log('about'); // DELETE
+    res.render('about');
 });
 
+// project routes
 app.get('/project/:id', (req, res) => {
-    console.log('project');
+    console.log('project'); // DELETE
+    res.render('project');
+});
+
+// Error Handling
+app.use((req, res, next) => {
+    const err = new Error('Sorry, this page doesn\'t exist..');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    console.log(`Status: ${err.status}, page not found.`);
+    res.render('error', err);
 });
 
 // app listens on port 3000
 app.listen(3000, () => {
-    console.log('The application is running on localhost:3000!');
+    console.log('The application is running on localhost:3000');
 });
